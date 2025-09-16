cb2f72d0c05d19dfd96aaa8376fc500c1463c5bc
ThalesCavalcanti <itthalescavalcanti20@gmail.com>
2025-09-16 18:25:18 -0300

    melhorando os teste lexicais

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from analisador_lexical import Lexer, LexicalError
 
-src = "int y = @123\nprint(x + .5)\n#comentario de linha\n/*comentario de bloco*/"
+src = "int y = 123+321*2/4-6 \n x>y != x<y == x<=y >= y \n a(b) = 1.5 + 2.0 \n print if else float int \n #comentario de linha unica \n /* comentario de \n multiplas linhas */ "
+src_erro = "@123"
 lex = Lexer(src)
 tokens = lex.tokenize()
 for t in tokens:
